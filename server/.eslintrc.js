6aa4f9b3935614d09561ee6007320aa3c11d44d3
Tranlehieu <66514357+Tranlehieu@users.noreply.github.com>
2024-03-28 15:26:57 +0700

    Add eslint-config-prettier

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -7,7 +7,8 @@ module.exports = {
     "extends": [
         "eslint:recommended",
         "plugin:react/recommended",
-        "plugin:react/jsx-runtime" /* from React 17 */
+        "plugin:react/jsx-runtime", /* from React 17 */
+        "prettier"
     ],
     "overrides": [
         {
